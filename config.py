475c175f64662a86235e57b9fe973e6105cf0422
Telkaaqt <galangabiyasa62@gmail.com>
2025-06-29 09:17:45 +0700

    config.py

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002122438222"))
 FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002447613053"))
 
 
-FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "0")) # auto delete in seconds
+FILE_AUTO_DELETE = int(os.getenv("FILE_AUTO_DELETE", "None")) # auto delete in seconds
 
 
 PORT = os.environ.get("PORT", "8080")
